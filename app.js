a0313581611c307bd71ec127ced336c91e8a895b
abnerfs <abnerfs@gmail.com>
2019-11-12 18:15:50 -0300

    Variable names

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ if(process.argv.length != 3)
 
 const valueWanted = parseFloat(process.argv[2]);
 
-const fixedTax = 0.6;
-const percTax = 4.79;
+const fixedFee = 0.6;
+const percFee = 4.79;
 
-const percCalc = (100 - percTax) / 100.0;
-const result =  (valueWanted + fixedTax) / percCalc;
+const percCalc = (100 - percFee) / 100.0;
+const result =  (valueWanted + fixedFee) / percCalc;
 
 console.log('Result: ' + result);
 console.log('Rounded result: ' + Math.ceil(result));
